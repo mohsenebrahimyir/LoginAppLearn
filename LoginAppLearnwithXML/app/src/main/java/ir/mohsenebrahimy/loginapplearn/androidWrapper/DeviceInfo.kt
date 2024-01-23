d4dd552a355e615df5a20cd8dee657ec81a2a325
mohsenebrahimyir <mohsenebrahimy.ir@mail.com>
2024-01-23 23:02:15 +0330

    add verifyCode in LoginApiService

---
+++ LoginAppLearnwithXML/app/src/main/java/ir/mohsenebrahimy/loginapplearn/androidWrapper/DeviceInfo.kt
@@ -0,0 +1,21 @@
+package ir.mohsenebrahimy.loginapplearn.androidWrapper
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+
+object DeviceInfo {
+    private var androidId: String? = null
+    @SuppressLint("HardwareIds")
+    fun getAndroidId(context: Context) : String {
+        if (androidId == null) {
+            androidId = Settings.Secure.getString(
+                context.contentResolver,
+                Settings.Secure.ANDROID_ID
+            )
+        }
+
+        return androidId ?: ""
+    }
+
+}
--- LoginAppLearnwithXML/app/src/main/java/ir/mohsenebrahimy/loginapplearn/remote/apiRepository/LoginApiService.kt
+++ LoginAppLearnwithXML/app/src/main/java/ir/mohsenebrahimy/loginapplearn/remote/apiRepository/LoginApiService.kt
@@ -4,6 +4,7 @@ import ir.mohsenebrahimy.loginapplearn.remote.dataModel.DefaultModel
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface LoginApiService {
@@ -12,6 +13,14 @@ interface LoginApiService {
     @POST("email/login")
     suspend fun sendRequest(
         @Field("email") email: String
+    ) : Response<DefaultModel>
+
+    @FormUrlEncoded
+    @POST("email/login/verify")
+    suspend fun verifyCode(
+        @Header("deviceUId")
+        @Field("code") code: String,
+        @Field("email") email: String,
     ) : Response<DefaultModel>
 
 }
